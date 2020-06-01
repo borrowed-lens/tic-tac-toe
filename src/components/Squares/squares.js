8442787168c342d85429b7e118f7eff664bd4fdf
Vivek <vivekprasad1410@gmail.com>
2020-06-01 13:29:11 +0530

    setting squares

---
+++ src/components/Squares/squares.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Square from '../square/square';
+
+const Squares = (props) => (
+    <>
+        <Square />
+    </>
+);
+export default Squares;
---
+++ src/components/square/square.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from './square.module.css';
+
+const Square = (props) => (
+    <div className={classes.Square}>
+        <p>X</p>
+    </div>
+);
+export default Square;
