668da70082624187e1b6d65d1c00621da5b020cd
Vivek <vivekprasad1410@gmail.com>
2020-06-04 21:53:56 +0530

    added styling, modal functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
 
 import classes from './App.module.css';
 import Squares from './components/Squares/squares';
@@ -22,12 +23,10 @@ class App extends Component {
         ];
         this.squareStates = { ...this.props.squares };
         this.player = 'X';
-        this.gameOver = false;
-    }
-    componentDidUpdate() {
-        if (!this.result && !this.gameOver) {
-            this.player = this.player === 'X' ? 'O' : 'X';
-        }
+        this.state = {
+            gameStart: false,
+            gameOver: false,
+        };
     }
     checkGameStatus = () => {
         for (let square in this.squareStates) {
@@ -46,40 +45,89 @@ class App extends Component {
                 this.squareStates[`square${b}`] ===
                     this.squareStates[`square${c}`]
             ) {
-                this.gameOver = true;
+                this.setState({
+                    gameOver: true,
+                });
                 return this.player;
             }
         }
-        this.gameOver = this.checkGameStatus();
+        this.setState({
+            gameOver: this.checkGameStatus(),
+        });
         return false;
+    };
+    startGameHandler = () => {
+        this.setState({
+            gameStart: true,
+        });
     };
     markPositionHandler = (position) => {
         if (!this.squareStates[position]) {
             this.squareStates[position] = this.player;
             this.winner = this.checkWinnerHandler();
             this.props.markPosition(position, this.player);
+            if (!this.result && !this.state.gameOver) {
+                this.player = this.player === 'X' ? 'O' : 'X';
+            }
         }
     };
     render() {
         return (
             <div className={classes.App}>
                 {/* <Backdrop /> */}
-                <Modal />
+                <Modal
+                    winner={this.winner}
+                    showModal={this.state.gameOver}
+                    close={this.closeModal}></Modal>
+                <CSSTransition
+                    in={this.state.gameStart}
+                    timeout={500}
+                    classNames={{
+                        enter: classes.PrimaryButton,
+                        enterActive: classes.PrimaryButtonActive,
+                        enterDone: classes.PrimaryButtonActive,
+                    }}>
+                    <button
+                        className={classes.PrimaryButton}
+                        onClick={this.startGameHandler}>
+                        start the game
+                    </button>
+                </CSSTransition>
                 {this.winner ? `winner is ${this.winner}` : ''}
-                {this.gameOver ? 'game over' : ''}
-                <div>
-                    <Squares
-                        row={1}
-                        onMarkPosition={this.markPositionHandler}
-                    />
-                    <Squares
-                        row={4}
-                        onMarkPosition={this.markPositionHandler}
-                    />
-                    <Squares
-                        row={7}
-                        onMarkPosition={this.markPositionHandler}
-                    />
+                {this.state.gameOver ? 'game over' : ''}
+                <div className={classes.SquareContainer}>
+                    <CSSTransition
+                        in={this.state.gameStart}
+                        timeout={500}
+                        classNames={{
+                            enter: classes.LeftDiv,
+                            enterActive: classes.LeftDivActive,
+                        }}>
+                        <div className={classes.LeftDiv}></div>
+                    </CSSTransition>
+                    <CSSTransition
+                        in={this.state.gameStart}
+                        timeout={500}
+                        classNames={{
+                            enter: classes.RightDiv,
+                            enterActive: classes.RightDivActive,
+                        }}>
+                        <div className={classes.RightDiv}></div>
+                    </CSSTransition>
+                    <div>
+                        <Squares
+                            row={1}
+                            onMarkPosition={this.markPositionHandler}
+                        />
+                        <Squares
+                            row={4}
+                            onMarkPosition={this.markPositionHandler}
+                        />
+                        <Squares
+                            row={7}
+                            onMarkPosition={this.markPositionHandler}
+                        />
+                    </div>
                 </div>
             </div>
         );
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import classes from './Modal.module.css';
 
-const Modal = (props) => 
-<div className={classes.Modal}>
-    <div></div>
-</div>;
+const Modal = (props) => {
+    let modalClasses = [classes.Modal, props.showModal ? classes.ModalOpen : null]
+    return (
+        <div className={modalClasses.join(' ')}>
+            <div>winner is {props.winner}</div>
+        </div>
+    );
+};
 export default Modal;
