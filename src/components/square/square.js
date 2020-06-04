e1fc66d27a12f7b2e596935c032cddfdbfb6b03a
Vivek <vivekprasad1410@gmail.com>
2020-06-04 18:26:03 +0530

    added transition for markers

--- src/components/square/square.js
+++ src/components/square/square.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { connect } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
 
 import classes from './square.module.css';
 
 const Square = (props) => {
+    let inProp = Boolean(props.squares[`square${props.value}`]);
+    let nodeRef = useRef(null);
     return (
         <div
             className={classes.Square}
             onClick={() => props.markPosition(`square${props.value}`)}>
-            <span>{props.squares[`square${props.value}`]}</span>
+            <CSSTransition
+                in={inProp}
+                timeout={300}
+                classNames={{
+                    enter: classes.SquareValueEnter,
+                    enterActive: classes.SquareValueEnterActive,
+                }}
+                nodeRef={nodeRef}>
+                <span ref={nodeRef}>{props.squares[`square${props.value}`]}</span>
+            </CSSTransition>
         </div>
     );
 };
