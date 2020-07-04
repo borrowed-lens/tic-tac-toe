fa009177765940c6b6568bb7db5dc9a595f74a9d
Vivek <vivekprasad1410@gmail.com>
2020-07-04 07:12:25 +0530

    restart button a11y

---
+++ build/precache-manifest.4e2cadaa312672883db7178130145db2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cbfc4dcdc36b414d5cff7612c382e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db16b307ceb9306efe8",
+    "url": "/static/css/main.b0669a6c.chunk.css"
+  },
+  {
+    "revision": "0072eecd7b30e5775e12",
+    "url": "/static/js/2.8be531b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8be531b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db16b307ceb9306efe8",
+    "url": "/static/js/main.8e5d0dbb.chunk.js"
+  },
+  {
+    "revision": "2adf10464682cab25afe",
+    "url": "/static/js/runtime-main.c0ef1627.js"
+  }
+]);
